b2c14fcf7ac8c2ddf275a7c71f21399c5735239b
cynthiatieno <cynthiatieno7@gmail.com>
2024-10-06 00:00:59 +0300

    volume of cylinder

---
+++ cylinder.py
@@ -0,0 +1,7 @@
+def volume_of_cylinder():
+    radius = float(input("Enter the radius: "))
+    height=float(input("Enter the height: "))
+    volume=3.1434567 * (radius ** 2) * height
+    print("f volume of cylinder is: {volume: .2f}")
+
+volume_of_cylinder
